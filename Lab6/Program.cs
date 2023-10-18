6fdd12b734b4bcf981ac158afca73d86ff5451ea
Shakejelly <fredrich93@gmail.com>
2023-10-18 14:41:24 +0200

    ..

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -7,10 +7,14 @@
             Elefant elefant = new Elefant();
             Moose moose = new Moose();
             Sloth sloth = new Sloth();
+
+            
+            
         }
         public void MakeSound()
         {
-
+            
+       
         }
     }
 }
